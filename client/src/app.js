ef4922df5ba09068d9fdeb9f9b017690bec6d684
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-04-17 12:23:39 +0100

    HTML form and event listener, image and some css

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+const FormView = require('./views/form_view.js')
+const ListView = require('./views/list_view.js');
+const BucketList = require('./models/model.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bucketListForm = document.querySelector('form#bucket-list-form');
+  // const FormView = new FormView(bucketListForm);
+  // FormView.bindEvents();
+
+
+});
